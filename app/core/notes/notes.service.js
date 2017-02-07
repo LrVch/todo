9c661b9768a9e86f748d439337a9d5a0d8eaa5cb
Roman Lonsky <lrvchgost@gmail.com>
2017-02-07 17:47:41 +0600

    test url

--- app/core/notes/notes.service.js
+++ app/core/notes/notes.service.js
@@ -4,7 +4,7 @@
 	angular.module('core.notes')
 		.factory('Notes', ['$http', function($http) {
 			console.log("notes service call")
-			  	return $http({method: 'GET', url: 'api/notes.json'});
+			  	return $http({method: 'GET', url: 'todo/dist/api/notes.json'});
 			}
 		]);
 }());
